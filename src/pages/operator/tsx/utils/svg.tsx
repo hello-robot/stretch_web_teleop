7fbe705e368c6f82a5aaa861f4c43601d54be725
Noah Ponto <ponton@uw.edu>
2023-05-25 08:44:45 -0700

    button renders with aspect ratio - removed sub video component - video passes aspect ratio to overlay button pad

--- src/pages/operator/tsx/utils/svg.tsx
+++ src/pages/operator/tsx/utils/svg.tsx
@@ -12,14 +12,15 @@ import gripRight from "operator/icons/Grip_Right.svg"
 import driveForward from "operator/icons/Drive_FWD.svg"
 import driveReverse from "operator/icons/Drive_RVS.svg"
 import { ButtonPadButton } from "../functionprovider/buttonpads"
+import { ButtonPadShape } from "../layoutcomponents/buttonpads"
 
 
-/**Resolution of SVG components */
+/** The pixel width of SVG components. */
 export const SVG_RESOLUTION = 500;
 
 /**
- * Takes a percentage value and returns the real pixel location on 
- * the SVG
+ * Takes a percentage value and returns a pixel value based on {@link SVG_RESOLUTION}
+ * 
  * @param percentage value between 0 and 100
  * @returns the pixel location
  * @example 0 -> 0, 50 -> resolution/2, 100 -> resolution
@@ -67,32 +68,51 @@ const DEFAULT_POSITION = {
 }
 
 /**
+ * Gets a list of path string descriptions for each button based on the {@link ButtonPadShape}
+ * 
+ * @param shape {@link ButtonPadShape} enum representing the shape of the button pad
+ * @returns a list of strings where each string is a path description for the shape of a single button
+ */
+export function getPathsFromShape(shape: ButtonPadShape, aspectRatio?: number): [string[], { x: number, y: number }[]] {
+    const width = SVG_RESOLUTION;
+    const height = aspectRatio ? SVG_RESOLUTION / aspectRatio : SVG_RESOLUTION;
+    switch (shape) {
+        case (ButtonPadShape.Directional):
+            return getDirectionalPaths(width, height);
+        case (ButtonPadShape.Realsense):
+            return getRealsenseManipPaths(width, height);
+        case (ButtonPadShape.Gripper):
+            return getGripperPaths(width, height);
+    }
+}
+
+/**
  * Directional button pad made up of four trapazoids around a box in the 
  * center of the button pad.
  * 
  * @param onRobot if the square should be around the robot, centered if false
  */
-export function getDirectionalPaths(onRobot: boolean = true): [string[], { x: number, y: number }[]] {
+function getDirectionalPaths(width: number, height: number, onRobot: boolean = true): [string[], { x: number, y: number }[]] {
     const boxPosition: BoxPosition = onRobot ? OVERHEAD_ROBOT_BASE : DEFAULT_POSITION;
-    const { centerX, centerY, height, width } = boxPosition;
-    const top = centerY - height / 2
-    const bot = centerY + height / 2
-    const lft = centerX - width / 2
-    const rgt = centerX + width / 2
+    const { centerX, centerY, height: boxHeight, width: boxWidth } = boxPosition;
+    const top = (centerY - boxHeight / 2) / width * height;
+    const bot = (centerY + boxHeight / 2) / width * height;
+    const lft = centerX - boxWidth / 2;
+    const rgt = centerX + boxWidth / 2;
 
-    const pathTop = `M 0 0 ${SVG_RESOLUTION} 0 ${rgt} ${top} ${lft} ${top} Z`
-    const pathRgt = `M ${SVG_RESOLUTION} 0 ${SVG_RESOLUTION} ${SVG_RESOLUTION} 
+    const pathTop = `M 0 0 ${width} 0 ${rgt} ${top} ${lft} ${top} Z`
+    const pathRgt = `M ${width} 0 ${width} ${height} 
                         ${rgt} ${bot} ${rgt} ${top} Z`
-    const pathBot = `M 0 ${SVG_RESOLUTION} ${SVG_RESOLUTION} ${SVG_RESOLUTION} 
+    const pathBot = `M 0 ${height} ${width} ${height} 
                         ${rgt} ${bot} ${lft} ${bot} Z`
-    const pathLft = `M 0 0 0 ${SVG_RESOLUTION} ${lft} ${bot} ${lft} ${top} Z`
+    const pathLft = `M 0 0 0 ${height} ${lft} ${bot} ${lft} ${top} Z`
 
     const paths = [pathTop, pathRgt, pathBot, pathLft]
     const iconPositions = [
         { x: centerX, y: top / 2 },
         { x: (SVG_RESOLUTION + rgt) / 2, y: centerY },
         { x: centerX, y: (SVG_RESOLUTION + bot) / 2 },
-        { x: (lft) / 2, y: centerY}
+        { x: (lft) / 2, y: centerY }
     ]
     return [paths, iconPositions];
 }
@@ -101,50 +121,50 @@ export function getDirectionalPaths(onRobot: boolean = true): [string[], { x: nu
  * Ordered: top left, top right, then top, bottom, left, right trapezoids, then 
  * top and bottom center buttons, and finally bottom left and bottom right.
  */
-export function getRealsenseManipPaths(): [string[], { x: number, y: number }[]] {
+function getRealsenseManipPaths(width: number, height: number): [string[], { x: number, y: number }[]] {
     /**Number of button layers from top to bottom in the display*/
     const numVerticalLayers = 6;
     /**How tall each layer of buttons should be.*/
-    const height = SVG_RESOLUTION / numVerticalLayers;
+    const layerHeight = height / numVerticalLayers;
     const centerWidth = percent2Pixel(30);
-    const centerLeft = (SVG_RESOLUTION - centerWidth) / 2;
+    const centerLeft = (width - centerWidth) / 2;
     const centerRight = centerLeft + centerWidth;
     const center = percent2Pixel(50);
     const paths = [
         // Top two buttons: left, right
-        rect(0, 0, center, height),
-        rect(center, 0, center, height),
+        rect(0, 0, center, layerHeight),
+        rect(center, 0, center, layerHeight),
         // Center directional trapezoid buttons: top, bottom, left, right
-        `M 0 ${height} ${SVG_RESOLUTION} ${height} ${centerRight} ${height * 2} 
-            ${centerLeft} ${height * 2} Z`,
-        `M 0 ${height * 5} ${SVG_RESOLUTION} ${height * 5} 
-            ${centerRight},${height * 4} ${centerLeft},${height * 4} Z`,
-        `M 0 ${height} 0 ${height * 5} ${centerLeft},${height * 4} 
-            ${centerLeft},${height * 2} Z`,
-        `M ${SVG_RESOLUTION} ${height} ${SVG_RESOLUTION} ${height * 5} 
-            ${centerRight},${height * 4} ${centerRight},${height * 2} Z`,
+        `M 0 ${layerHeight} ${width} ${layerHeight} ${centerRight} ${layerHeight * 2} 
+            ${centerLeft} ${layerHeight * 2} Z`,
+        `M 0 ${layerHeight * 5} ${height} ${layerHeight * 5} 
+            ${centerRight},${layerHeight * 4} ${centerLeft},${layerHeight * 4} Z`,
+        `M 0 ${layerHeight} 0 ${layerHeight * 5} ${centerLeft},${layerHeight * 4} 
+            ${centerLeft},${layerHeight * 2} Z`,
+        `M ${width} ${layerHeight} ${width} ${layerHeight * 5} 
+            ${centerRight},${layerHeight * 4} ${centerRight},${layerHeight * 2} Z`,
         // // Center two rectangle buttons: top, bottom
-        rect(centerLeft, height * 2, centerWidth, height),
-        rect(centerLeft, height * 3, centerWidth, height),
+        rect(centerLeft, layerHeight * 2, centerWidth, layerHeight),
+        rect(centerLeft, layerHeight * 3, centerWidth, layerHeight),
         // // Bottom two buttons: left, right
-        rect(0, height * 5, center, height),
-        rect(center, height * 5, center, height)
+        rect(0, layerHeight * 5, center, layerHeight),
+        rect(center, layerHeight * 5, center, layerHeight)
     ]
     const iconPositions = [
         // Top two
-        { x: center / 2, y: height / 2 },
-        { x: (SVG_RESOLUTION + center) / 2, y: height / 2 },
+        { x: center / 2, y: layerHeight / 2 },
+        { x: (width + center) / 2, y: layerHeight / 2 },
         // Center directional trapezoid buttons
-        { x: SVG_RESOLUTION / 2, y: height * 3 / 2 },
-        { x: SVG_RESOLUTION / 2, y: height * 9 / 2 },
-        { x: centerLeft / 2, y: height * 6 / 2 },
-        { x: (SVG_RESOLUTION + centerRight) / 2, y: height * 6 / 2 },
+        { x: width / 2, y: layerHeight * 3 / 2 },
+        { x: width / 2, y: layerHeight * 9 / 2 },
+        { x: centerLeft / 2, y: layerHeight * 6 / 2 },
+        { x: (width + centerRight) / 2, y: layerHeight * 6 / 2 },
         // Center two rectangle buttons
-        { x: SVG_RESOLUTION / 2, y: height * 5 / 2 },
-        { x: SVG_RESOLUTION / 2, y: height * 7 / 2 },
+        { x: width / 2, y: layerHeight * 5 / 2 },
+        { x: width / 2, y: layerHeight * 7 / 2 },
         // Bottom two buttons
-        { x: center / 2, y: height * 11 / 2 },
-        { x: (SVG_RESOLUTION + center) / 2, y: height * 11 / 2 }
+        { x: center / 2, y: layerHeight * 11 / 2 },
+        { x: (width + center) / 2, y: layerHeight * 11 / 2 }
     ]
     return [paths, iconPositions];
 }
@@ -152,26 +172,28 @@ export function getRealsenseManipPaths(): [string[], { x: number, y: number }[]]
 /**
  * Ordered top, botton, left, right, larger center, smaller center
  */
-export function getGripperPaths(): [string[], { x: number, y: number }[]] {
+function getGripperPaths(width: number, height: number): [string[], { x: number, y: number }[]] {
     /**Number of button layers from top to bottom in the display*/
     const numLayers = 5;
+    /**How wide each layer of buttons should be.*/
+    const xMargin = width / numLayers;
     /**How tall each layer of buttons should be.*/
-    const margin = SVG_RESOLUTION / numLayers;
+    const yMargin = height / numLayers;
     const paths = [
-        rect(0, 0, SVG_RESOLUTION, margin),  // top
-        rect(0, SVG_RESOLUTION - margin, SVG_RESOLUTION, margin),  // bottom
-        rect(0, margin, margin, margin * 3),  // left
-        rect(SVG_RESOLUTION - margin, margin, margin, margin * 3), // right
-        rect(margin, margin, margin * 3, margin * 3),  // gripper open
-        rect(margin * 2, margin * 2, margin, margin)  // gripper close
+        rect(0, 0, width, yMargin),  // top
+        rect(0, height - yMargin, width, yMargin),  // bottom
+        rect(0, yMargin, xMargin, yMargin * 3),  // left
+        rect(width - xMargin, yMargin, xMargin, yMargin * 3), // right
+        rect(xMargin, yMargin, xMargin * 3, yMargin * 3),  // gripper open
+        rect(xMargin * 2, yMargin * 2, xMargin, yMargin)  // gripper close
     ]
     const iconPositions = [
-        { x: SVG_RESOLUTION / 2, y: margin / 2 },  // top
-        { x: SVG_RESOLUTION / 2, y: (2 * SVG_RESOLUTION - margin) / 2 },  // bottom
-        { x: margin / 2, y: SVG_RESOLUTION / 2 },  // left
-        { x: (2 * SVG_RESOLUTION - margin) / 2, y: SVG_RESOLUTION / 2 },  // right
-        { x: margin * 7 / 2, y: SVG_RESOLUTION / 2 },  // gripper open
-        { x: SVG_RESOLUTION / 2, y: SVG_RESOLUTION / 2 },  // gripper close
+        { x: width / 2, y: yMargin / 2 },  // top
+        { x: width / 2, y: (2 * height - yMargin) / 2 },  // bottom
+        { x: yMargin / 2, y: height / 2 },  // left
+        { x: (2 * width - yMargin) / 2, y: height / 2 },  // right
+        { x: yMargin * 7 / 2, y: height / 2 },  // gripper open
+        { x: width / 2, y: height / 2 },  // gripper close
     ]
     return [paths, iconPositions];
 }
