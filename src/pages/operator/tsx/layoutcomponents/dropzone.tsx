edeb3f044674231a969ca989595dd0eb70da96c3
Vinitha <vinitha.ranganeni@gmail.com>
2023-05-31 19:35:10 -0700

    changed drop zone style

--- src/pages/operator/tsx/layoutcomponents/dropzone.tsx
+++ src/pages/operator/tsx/layoutcomponents/dropzone.tsx
@@ -73,7 +73,7 @@ export const DropZone = (props: DropZoneProps) => {
             hidden={!isActive}
             onClick={handleClick}
         >
-            vertical_align_bottom
+            save_alt
         </span>
     )
 }
