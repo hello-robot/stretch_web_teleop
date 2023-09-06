c3efc152e567f0bf6b1a724ba75d326af287607f
hello-chintan <cdesai@hello-robot.com>
2023-09-06 16:41:54 -0700

    Add usb cam launch files for gripper and nav cams

---
+++ launch/navigation_camera.launch.py
@@ -0,0 +1,37 @@
+import launch
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='usb_cam',
+            executable='usb_cam_node_exe',
+            name='navigation_camera',
+            output='screen',
+            parameters=[
+                {'video_device': '/dev/hello-navigation-camera'},
+                {'image_width': 1024},
+                {'image_height': 768},
+                {'framerate': 15.0},
+                {'pixel_format': 'yuyv'},
+                {'brightness': -40},
+                {'contrast': 40},
+                {'hue': 0},
+                {'saturation': 60},
+                {'sharpness': 100},
+                {'autoexposure': True},
+                {'exposure': 150},
+                {'auto_white_balance': False},
+                {'white_balance': 4250},
+                {'gain': 50},
+                {'camera_frame_id': 'navigation_camera'},
+                {'camera_name': 'navigation_camera'},
+                {'io_method': 'mmap'}
+            ],
+            remappings=[('/usb_cam/image_raw', '/navigation_camera/image_raw')]
+        ),
+        # You can add more nodes or configurations here if needed.
+    ])
