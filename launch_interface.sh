ca175464d3d828e1de0b0432b572ca8591ac9fda
Vinitha Ranganeni <vinitha@hello-robot.com>
2023-10-26 09:32:09 -0700

    updated scripts to follow new install standard paths

--- launch_interface.sh
+++ launch_interface.sh
@@ -3,23 +3,10 @@
 pkill -f python;
 sleep 2;
 udevadm control --reload-rules && udevadm trigger
-kill -9 $(sudo lsof -t -i:80)
-pkill -f "node ./node_modules/.bin/nodemon server.js"
-# ros2 daemon stop;
-# ros2 daemon start;
-# sudo killall -r ros;
-# sudo killall -r start_web_server_and_robot_browser;
 source /opt/ros/humble/setup.bash
 source /home/hello-robot/ament_ws/install/setup.bash
-source /home/hello-robot/hello_robot_ws/install/setup.bash
+source /home/hello-robot/web_interface_ws/install/setup.bash
 source /usr/share/colcon_cd/function/colcon_cd.sh
-screen -dm -S "ros" ros2 launch stretch_teleop_interface web_interface.launch.py certfile:=stretch-re2-2051-local+6.pem keyfile:=stretch-re2-2051-local+6-key.pem map_yaml:=map.yaml 
+screen -dm -S "ros" ros2 launch stretch_teleop_interface web_interface.launch.py certfile:=${HELLO_FLEET_ID}+6.pem keyfile:=${HELLO_FLEET_ID}+6-key.pem map_yaml:=map.yaml 
 sleep 3;
-/home/hello-robot/hello_robot_ws/src/stretch_teleop_interface/start_web_server_and_robot_browser.sh
-# sleep 5;
-# export NODE_EXTRA_CA_CERTS="/home/hello-robot/hello_robot_ws/src/stretch_teleop_interface/certificates/rootCA.pem"
-# cd /home/hello-robot/hello_robot_ws/src/stretch_teleop_interface && npm run localstorage &
-# sudo --preserve-env /home/hello-robot/hello_robot_ws/src/stretch_teleop_interface/node_modules/.bin/nodemon server.js &
-# /home/hello-robot/hello_robot_ws/src/stretch_teleop_interface/node_modules/.bin/nodemon start_robot_browser.js
-# set +x
-# ros2 launch stretch_core d435i_high_resolution.launch.py
+/home/hello-robot/web_interface_ws/src/stretch_teleop_interface/start_web_server_and_robot_browser.sh
--- start_web_server_and_robot_browser.sh
+++ start_web_server_and_robot_browser.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# PORT_NUMBER=3000
-# lsof -i tcp:${PORT_NUMBER} | awk 'NR!=1 {print $2}' | xargs kill 
-# kill $(lsof -t -i:3000)
-
-# sudo kill -9 $(sudo lsof -t -i:80)
-# sudo pkill -f "node ./node_modules/.bin/nodemon server.js"
-
-# export NODE_EXTRA_CA_CERTS="$(readlink -f ./certificates)/rootCA.pem"
-
-# npm run $1 &
-# ./node_modules/.bin/nodemon server.js &
-# ./node_modules/.bin/nodemon start_robot_browser.js
-# set +x
-
-export NODE_EXTRA_CA_CERTS="/home/hello-robot/hello_robot_ws/src/stretch_teleop_interface/certificates/rootCA.pem"
-# cd /home/hello-robot/hello_robot_ws/src/stretch_teleop_interface && npm run localstorage &
-cd /home/hello-robot/hello_robot_ws/src/stretch_teleop_interface && pm2 start npm --name="stretch_teleop_interface" -- run localstorage 
-cd /home/hello-robot/hello_robot_ws/src/stretch_teleop_interface && pm2 start server.js --watch
-cd /home/hello-robot/hello_robot_ws/src/stretch_teleop_interface && pm2 start start_robot_browser.js --watch 
+export NODE_EXTRA_CA_CERTS="/home/hello-robot/web_interface_ws/src/stretch_teleop_interface/certificates/rootCA.pem"
+cd /home/hello-robot/web_interface_ws/src/stretch_teleop_interface && pm2 start npm --name="stretch_teleop_interface" -- run localstorage 
+cd /home/hello-robot/web_interface_ws/src/stretch_teleop_interface && pm2 start server.js --watch
+cd /home/hello-robot/web_interface_ws/src/stretch_teleop_interface && pm2 start start_robot_browser.js --watch 
 set +x
