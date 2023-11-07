c10ec5be4327f74d8f3fad106b9af28f00d62d3e
Vinitha Ranganeni <vinitha@hello-robot.com>
2023-11-07 12:23:19 -0800

    added image rotate for dex wrist camera so image is always facing up

--- nodes/configure_video_streams.py
+++ nodes/configure_video_streams.py
@@ -57,7 +57,7 @@ class ConfigureVideoStreams(Node):
         self.camera_rgb_subscriber =  message_filters.Subscriber(self, Image, "/camera/color/image_raw")
         self.overhead_camera_rgb_subscriber = self.create_subscription(Image, "/navigation_camera/image_raw", self.navigation_camera_cb, QoSProfile(depth=1, reliability=ReliabilityPolicy.RELIABLE))
         self.gripper_camera_rgb_subscriber = self.create_subscription(Image, "/gripper_camera/color/image_rect_raw", self.gripper_camera_cb, 1)
-        #self.joint_state_subscription = self.create_subscription(JointState, "/stretch/joint_states", self.joint_state_cb, 1)
+        self.joint_state_subscription = self.create_subscription(JointState, "/stretch/joint_states", self.joint_state_cb, 1)
         self.point_cloud_subscriber =  message_filters.Subscriber(self, PointCloud2, "/camera/depth/color/points")
         self.camera_info_subscriber = self.create_subscription(CameraInfo, "/camera/aligned_depth_to_color/camera_info", self.camera_info_cb, 1)
         # self.aruco_markers_subscriber = message_filters.Subscriber(self, MarkerArray, "/aruco/marker_array")
@@ -277,9 +277,9 @@ class ConfigureVideoStreams(Node):
     def gripper_camera_cb(self, ros_image):
         image = self.cv_bridge.imgmsg_to_cv2(ros_image)
         for image_config_name in self.gripper_params:
-            # img = self.rotate_image_around_center(image, self.roll_value)
+            img = self.rotate_image_around_center(image, -1*self.roll_value)
             self.gripper_images[image_config_name] = \
-                self.configure_images(image, self.gripper_params[image_config_name])
+                self.configure_images(img, self.gripper_params[image_config_name])
         self.gripper_camera_rgb_image = self.gripper_images[self.camera_perspective["gripper"]]
         self.publish_compressed_msg(self.gripper_camera_rgb_image, self.publisher_gripper_cmp)
 
@@ -299,8 +299,9 @@ class ConfigureVideoStreams(Node):
         return result
 
     def joint_state_cb(self, joint_state):
-        roll_index = joint_state.name.index('joint_wrist_roll')
-        self.roll_value = joint_state.position[roll_index]
+        if 'joint_wrist_roll' in joint_state.name:
+            roll_index = joint_state.name.index('joint_wrist_roll')
+            self.roll_value = joint_state.position[roll_index]
         
     def publish_compressed_msg(self, image, publisher):
         msg = CompressedImage()
