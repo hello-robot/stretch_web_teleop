da901d54d94d3ea300f3a426e112b5e890119845
Binit Shah <bshah@hello-robot.com>
2024-02-28 14:45:34 -0500

    Quiet verbose output from pm2

--- start_web_server_and_robot_browser.sh
+++ start_web_server_and_robot_browser.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export NODE_EXTRA_CA_CERTS="/home/hello-robot/ament_ws/src/stretch_web_teleop/certificates/rootCA.pem"
-cd ~/ament_ws/src/stretch_web_teleop && pm2 start npm --name="stretch_web_teleop" -- run localstorage 
-cd ~/ament_ws/src/stretch_web_teleop && pm2 start server.js --watch
-cd ~/ament_ws/src/stretch_web_teleop && pm2 start start_robot_browser.js --watch 
+cd ~/ament_ws/src/stretch_web_teleop && pm2 start -s npm --name="stretch_web_teleop" -- run localstorage
+cd ~/ament_ws/src/stretch_web_teleop && pm2 start -s server.js --watch
+cd ~/ament_ws/src/stretch_web_teleop && pm2 start -s start_robot_browser.js --watch
 set +x
 echo ""
 echo "Visit the URL(s) below to see the web interface:"
