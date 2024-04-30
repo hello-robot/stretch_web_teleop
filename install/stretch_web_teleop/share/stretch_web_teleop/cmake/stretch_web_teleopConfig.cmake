95dfed63aacf7341e4e86e18ab29724eb458affb
Mohamed Fazil <mfazil@hello-robot.com>
2024-04-29 23:07:45 -0700

    d405 depth module params fix

---
+++ install/stretch_web_teleop/share/stretch_web_teleop/cmake/stretch_web_teleopConfig.cmake
@@ -0,0 +1,42 @@
+# generated from ament/cmake/core/templates/nameConfig.cmake.in
+
+# prevent multiple inclusion
+if(_stretch_web_teleop_CONFIG_INCLUDED)
+  # ensure to keep the found flag the same
+  if(NOT DEFINED stretch_web_teleop_FOUND)
+    # explicitly set it to FALSE, otherwise CMake will set it to TRUE
+    set(stretch_web_teleop_FOUND FALSE)
+  elseif(NOT stretch_web_teleop_FOUND)
+    # use separate condition to avoid uninitialized variable warning
+    set(stretch_web_teleop_FOUND FALSE)
+  endif()
+  return()
+endif()
+set(_stretch_web_teleop_CONFIG_INCLUDED TRUE)
+
+# output package information
+if(NOT stretch_web_teleop_FIND_QUIETLY)
+  message(STATUS "Found stretch_web_teleop: 0.1.0 (${stretch_web_teleop_DIR})")
+endif()
+
+# warn when using a deprecated package
+if(NOT "" STREQUAL "")
+  set(_msg "Package 'stretch_web_teleop' is deprecated")
+  # append custom deprecation text if available
+  if(NOT "" STREQUAL "TRUE")
+    set(_msg "${_msg} ()")
+  endif()
+  # optionally quiet the deprecation message
+  if(NOT ${stretch_web_teleop_DEPRECATED_QUIET})
+    message(DEPRECATION "${_msg}")
+  endif()
+endif()
+
+# flag package as ament-based to distinguish it after being find_package()-ed
+set(stretch_web_teleop_FOUND_AMENT_PACKAGE TRUE)
+
+# include all config extra files
+set(_extras "")
+foreach(_extra ${_extras})
+  include("${stretch_web_teleop_DIR}/${_extra}")
+endforeach()
