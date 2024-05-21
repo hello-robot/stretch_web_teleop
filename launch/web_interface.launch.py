f9d037ebb78f54fe602bdd63b1e8cf292a571e17
Hello Robot Inc <hello-robot@stretch-re2-2054.lan>
2024-05-20 18:27:17 -0700

    Added Stretch 3 configuration with tablet to launch script

--- launch/web_interface.launch.py
+++ launch/web_interface.launch.py
@@ -78,8 +78,12 @@ def map_configuration_to_drivers(model, tool, has_beta_teleop_kit, has_nav_head_
     # Stretch 2+ (upgraded Stretch 2)
     elif model == "RE2V0" and tool == "eoa_wrist_dw3_tool_sg3" and has_beta_teleop_kit == False and has_nav_head_cam == True:
         return 'both'     , False, False, True
+    elif model == "RE2V0" and tool == "eoa_wrist_dw3_tool_nil" and has_beta_teleop_kit == False and has_nav_head_cam == True:
+        return 'both'     , False, False, True
     # Stretch 3
     elif model == "SE3"   and tool == "eoa_wrist_dw3_tool_sg3" and has_beta_teleop_kit == False and has_nav_head_cam == True:
+        return 'both'     , False, False, True
+    elif model == "SE3"   and tool == "eoa_wrist_dw3_tool_nil" and has_beta_teleop_kit == False and has_nav_head_cam == True:
         return 'both'     , False, False, True
 
     raise ValueError(f'cannot find valid configuration for model={model}, tool={tool}, has_beta_teleop_kit={has_beta_teleop_kit}, has_nav_head_cam={has_nav_head_cam}')
