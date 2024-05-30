c9a7d9aba8ba7228139652b4c1854ff1baf37eec
Amal Nanavati <amaln@hello-robot.com>
2024-05-29 18:21:03 -0700

    [WIP] Computing the Jacobian for arm lift works

--- nodes/move_to_pregrasp.py
+++ nodes/move_to_pregrasp.py
@@ -1,27 +1,74 @@
 #!/usr/bin/env python3
 
 # Standard Imports
+import os
+import subprocess
 import sys
 import threading
 from typing import Dict, List, Optional, Tuple
 
 # Third-party Imports
+from ament_index_python.packages import get_package_share_directory
+from builtin_interfaces.msg import Time
+from geometry_msgs.msg import Point, Quaternion
 import message_filters
 import numpy as np
+import numpy.typing as npt
 import pcl
+import PyKDL as kdl
 import rclpy
-import ros2_numpy
-import yaml
 from rclpy.action import ActionServer, CancelResponse, GoalResponse
 from rclpy.action.server import ServerGoalHandle
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.duration import Duration
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
-from sensor_msgs.msg import CameraInfo, Image, PointCloud2
+import ros2_numpy
+from sensor_msgs.msg import CameraInfo, Image, JointState, PointCloud2
+from std_msgs.msg import Header
+import stretch_body.robot as rb
+from tf2_geometry_msgs import PoseStamped
+import tf2_py as tf2
+import tf2_ros
+from tf_transformations import euler_from_quaternion
+import yaml
 
 # Local Imports
+import normalized_velocity_control as nvc
 from stretch_web_teleop.action import MoveToPregrasp
+from urdf import treeFromString
 
+
+def jacobian_to_np(J: kdl.Jacobian) -> npt.NDArray[np.float64]:
+    """
+    Convert a KDL Jacobian to a numpy array.
+
+    Parameters
+    ----------
+    J: The KDL Jacobian.
+
+    Returns
+    -------
+    npt.NDArray[np.float64]: The Jacobian as a numpy array.
+    """
+    return np.array([[J[i, j] for j in range(J.columns())] for i in range(J.rows())])
+
+def np_to_joint_array(joint_state: npt.NDArray[np.float64]) -> kdl.JntArray:
+    """
+    Convert a numpy array to a KDL joint array.
+
+    Parameters
+    ----------
+    joint_state: The numpy array.
+
+    Returns
+    -------
+    kdl.JntArray: The KDL joint array.
+    """
+    q = kdl.JntArray(len(joint_state))
+    for i, q_i in enumerate(joint_state):
+        q[i] = q_i
+    return q
 
 class MoveToPregraspNode(Node):
     """
@@ -31,6 +78,8 @@ class MoveToPregraspNode(Node):
     aligned with the clicked pixel, making it easy for the user to grasp
     the object the pixel is a part of.
     """
+    BASE_LINK = "base_link"
+    END_EFFECTOR_LINK = "link_grasp_center"
 
     def __init__(self, image_params_file: str):
         """
@@ -47,6 +96,22 @@ class MoveToPregraspNode(Node):
         # Load the video stream parameters
         with open(image_params_file, "r") as params:
             self.image_params = yaml.safe_load(params)
+
+        # Load the Jacobian solver
+        # TODO: Reject goal if the below are still None/empty
+        self.jacobian_solver = None
+        self.jacobian_joint_order = []
+        self.K = None
+        self.load_jacobian_solver()
+        
+        # Use masks to limit which joints we move and which cartesian dimensions we control
+        # For starters, only control joint lift and z.
+        self.joint_mask = np.array([joint_name == "joint_lift" for joint_name in self.jacobian_joint_order])
+        self.cartesian_mask = np.array([False, False, True, False, False, False])
+
+        # Initialize TF2
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
         # Subscribe to the Realsense's RGB, pointcloud, and camera info feeds
         self.latest_realsense_msgs_lock = threading.Lock()
@@ -86,6 +151,17 @@ class MoveToPregraspNode(Node):
             callback_group=MutuallyExclusiveCallbackGroup(),
         )
 
+        # Subscribe to the robot's current joint state
+        self.latest_joint_state_lock = threading.Lock()
+        self.latest_joint_state = None
+        self.joint_state_subscriber = self.create_subscription(
+            JointState,
+            "/joint_states",
+            self.joint_state_cb,
+            qos_profile=1,
+            callback_group=MutuallyExclusiveCallbackGroup(),
+        )
+
         # Create the shared resource to ensure that the action server rejects all
         # new goals while a goal is currently active.
         self.active_goal_request_lock = threading.Lock()
@@ -101,6 +177,85 @@ class MoveToPregraspNode(Node):
             cancel_callback=self.cancel_callback,
             callback_group=MutuallyExclusiveCallbackGroup(),
         )
+
+    def load_jacobian_solver(self) -> bool:
+        """
+        Load the Jacobian solver for the robot.
+        """
+        # Get the path to the XACRO file
+        # TODO: Only enable this feature on the Stretch 3, modify the web app and launch file accordingly
+        stretch_description_dir = get_package_share_directory("stretch_description")
+        urdf_file = os.path.join(
+            stretch_description_dir, 
+            "urdf/stretch_description_SE3_eoa_wrist_dw3_tool_sg3.xacro",
+        )
+        # Get the URDF string
+        urdf_string = subprocess.check_output(["xacro", urdf_file])
+        # Load the URDF
+        ok, robot = treeFromString(urdf_string)
+        if not ok:
+            self.get_logger().error(f"Failed to load the URDF at {urdf_file}")
+            return False
+
+        # Get the original chain
+        orig_chain = robot.getChain(self.BASE_LINK, self.END_EFFECTOR_LINK)
+
+        # Add dummy links and joints for base translation and revolution
+        chain = kdl.Chain()
+        chain.addSegment(kdl.Segment(
+            name="link_base_translation",
+            joint=kdl.Joint(
+                name="joint_base_translation",
+                origin=kdl.Vector(0, 0, 0),
+                axis=kdl.Vector(1, 0, 0),
+                type=kdl.Joint.TransAxis,
+            ),
+            f_tip=kdl.Frame(),
+            I=kdl.RigidBodyInertia(
+                m=1.0,
+                oc=kdl.Vector(0, 0, 0),
+                Ic=kdl.RotationalInertia(
+                    1.0, 1.0, 1.0, 0.0, 0.0, 0.0
+                ),
+            ),
+        ))
+        chain.addSegment(kdl.Segment(
+            name="link_base_revolution",
+            joint=kdl.Joint(
+                name="joint_base_revolution",
+                origin=kdl.Vector(0, 0, 0),
+                axis=kdl.Vector(1, 0, 0),
+                type=kdl.Joint.RotAxis,
+            ),
+            f_tip=kdl.Frame(),
+            I=kdl.RigidBodyInertia(
+                m=1.0,
+                oc=kdl.Vector(0, 0, 0),
+                Ic=kdl.RotationalInertia(
+                    1.0, 1.0, 1.0, 0.0, 0.0, 0.0
+                ),
+            ),
+        ))
+        chain.addChain(orig_chain)
+        self.chain = chain
+
+        # Get the Jacobian solver
+        self.jacobian_solver = kdl.ChainJntToJacSolver(self.chain)
+
+        # Get the joint order
+        self.jacobian_joint_order = []
+        for i in range(self.chain.getNrOfSegments()):
+            joint = self.chain.getSegment(i).getJoint()
+            if joint.getType() != kdl.Joint.Fixed:
+                self.jacobian_joint_order.append(joint.getName())
+
+        # Store the control gains
+        self.K = np.eye(len(self.jacobian_joint_order), dtype=np.float64)
+        for i, joint_name in enumerate(self.jacobian_joint_order):
+            if joint_name in ["joint_arm_l3", "joint_arm_l2", "joint_arm_l1", "joint_arm_l0"]:
+                self.K[i, i] = 0.2
+
+        return True
 
     def camera_info_cb(self, msg: CameraInfo) -> None:
         """
@@ -124,6 +279,21 @@ class MoveToPregraspNode(Node):
         """
         with self.latest_realsense_msgs_lock:
             self.latest_realsense_msgs = (rgb_msg, pointcloud_msg)
+
+    def joint_state_cb(self, msg: JointState) -> None:
+        """
+        Callback for the joint state subscriber. Save the latest joint state message.
+
+        Parameters
+        ----------
+        msg: The joint state message.
+        """
+        with self.latest_joint_state_lock:
+            self.latest_joint_state = [
+                msg.position[msg.name.index(joint_name)]
+                if joint_name in msg.name else 0.0 # TODO: Setting base translation and revolution to 0.0 seems suspect
+                for joint_name in self.jacobian_joint_order
+            ]
 
     def goal_callback(self, goal_request: MoveToPregrasp.Goal) -> GoalResponse:
         """
@@ -225,30 +395,62 @@ class MoveToPregraspNode(Node):
         else:
             u, v = raw_u, raw_v
 
-        # Deproject the clicked pixel to get the 3D coordinates of the clicked point
+        # Get the latest Realsense messages
         with self.latest_realsense_msgs_lock:
             rgb_msg, pointcloud_msg = self.latest_realsense_msgs
         pointcloud = ros2_numpy.point_cloud2.pointcloud2_to_xyz_array(
             pointcloud_msg
         )  # N x 3 array
-        ray_dir = np.linalg.pinv(self.p)[:3, :] @ np.array(
-            [u, v, 1]
-        )  # The direction of the pixel from the camera
-        ray_dir /= np.linalg.norm(ray_dir)
-        # Find the pointcloud point that is closest to the ray
-        p, r = pointcloud, ray_dir
-        closest_point_idx = np.argmin(
-            np.linalg.norm(
-                p - np.multiply((p @ r).reshape((-1, 1)), r.reshape((1, 3))), axis=1
-            )
-        )
+
+        # Deproject the clicked pixel to get the 3D coordinates of the clicked point
+        x, y, z = self.deproject_pixel_to_point(u, v, pointcloud)
         self.get_logger().info(
-            f"Closest point to clicked pixel (camera frame): {p[closest_point_idx]}"
+            f"Closest point to clicked pixel (camera frame): {(x, y, z)}"
         )
+
+        # Get the goal end effector pose
+        goal_pose = self.get_goal_pose(x, y, z, pointcloud_msg.header)
+
+        # Keep moving the robot until the end effector is aligned with the clicked pixel
+        rate = self.create_rate(10)
+        err = None
+        while err is None or not np.all(np.isclose(err, 0.0, atol=1e-2)):
+            ok, err = self.get_err(goal_pose)
+            if not ok:
+                err = None
+                continue
+            # TODO: test for cancellations and rclpy shutdown and such. And TF lookup errors.
+            self.get_logger().info(f"Error: {err}")
+
+            # Get the current joint state
+            with self.latest_joint_state_lock:
+                q = np_to_joint_array(self.latest_joint_state)
+            self.get_logger().info(f"Joint State: {list(zip(self.jacobian_joint_order, self.latest_joint_state))}")
+
+            # Get the Jacobian matrix for the chain
+            J = kdl.Jacobian(len(self.jacobian_joint_order))
+            self.jacobian_solver.JntToJac(q, J)
+            J = jacobian_to_np(J)
+
+            # Calculate the pseudo-inverse of the Jacobian
+            J_pinv = np.linalg.pinv(J)
+            self.get_logger().info(f"Jacobian Pseudo-Inverse: {J_pinv}")
+            
+            # Apply the mask
+            J_pinv[:, np.logical_not(self.cartesian_mask),] = 0.0
+            J_pinv[np.logical_not(self.joint_mask), :] = 0.0
+            self.get_logger().info(f"Jacobian Pseudo-Inverse (Masked): {J_pinv}")
+
+            # Calculate the joint velocities
+            joint_velocities = self.K @ (J_pinv @ err)
+            self.get_logger().info(f"Joint Velocities: {list(zip(self.jacobian_joint_order, joint_velocities))}")
+            
+            rate.sleep()
 
         goal_handle.succeed()
         result = MoveToPregrasp.Result()
         self.active_goal_request = None
+        self.get_logger().info("Goal succeeded!")
         return result
 
     def inverse_transform_pixel(self, u: int, v: int, params: Dict) -> Tuple[int, int]:
@@ -298,6 +500,144 @@ class MoveToPregraspNode(Node):
 
         return u, v
 
+    def deproject_pixel_to_point(
+        self, u: int, v: int, pointcloud: npt.NDArray[np.float32]
+    ) -> Tuple[float, float, float]:
+        """
+        Deproject the clicked pixel to get the 3D coordinates of the clicked point.
+
+        Parameters
+        ----------
+        u: The horizontal coordinate of the clicked pixel.
+        v: The vertical coordinate of the clicked pixel.
+        pointcloud: The pointcloud array of size (N, 3).
+
+        Returns
+        -------
+        Tuple[float, float, float]: The 3D coordinates of the clicked point.
+        """
+        # Get the ray from the camera origin to the clicked point
+        ray_dir = np.linalg.pinv(self.p)[:3, :] @ np.array(
+            [u, v, 1]
+        )
+        ray_dir /= np.linalg.norm(ray_dir)
+
+        # Find the point that is closest to the ray
+        p, r = pointcloud, ray_dir
+        closest_point_idx = np.argmin(
+            np.linalg.norm(
+                p - np.multiply((p @ r).reshape((-1, 1)), r.reshape((1, 3))), axis=1
+            )
+        )
+
+        return p[closest_point_idx]
+
+    def get_goal_pose(
+        self, x: float, y: float, z: float, header: Header
+    ) -> PoseStamped:
+        """
+        Get the goal end effector pose.
+
+        Parameters
+        ----------
+        x: The x-coordinate of the clicked point.
+        y: The y-coordinate of the clicked point.
+        z: The z-coordinate of the clicked point.
+        header: The header of the pointcloud message.
+
+        Returns
+        -------
+        PoseStamped: The goal end effector pose.
+        """
+        goal_pose = PoseStamped()
+        goal_pose.header = header
+        goal_pose.pose.position = Point(x=x, y=y, z=z)
+        goal_pose.pose.orientation = Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
+        return goal_pose
+
+    def get_err(self, goal_pose: PoseStamped, use_mask: bool = True) -> Tuple[bool, npt.NDArray[np.float64]]:
+        """
+        Get the error between the goal pose and the current end effector pose.
+        Returns the error in base link frame.
+
+        Parameters
+        ----------
+        goal_pose: The goal end effector pose.
+        use_mask: Whether to apply the cartesian mask before outputting the error.
+
+        Returns
+        -------
+        bool: Whether the error was successfully calculated.
+        npt.NDArray[np.float64]: The error in base link frame. The error is a 6D vector
+            consisting of the translation and rotation errors.
+        """
+        timeout_secs = 0.5
+
+        # Get the current end effector pose in base frame
+        try:
+            ee_transform = self.tf_buffer.lookup_transform(
+                self.BASE_LINK, self.END_EFFECTOR_LINK, Time(), timeout=Duration(seconds=timeout_secs),
+            )
+        except (
+            tf2.ConnectivityException,
+            tf2.ExtrapolationException,
+            tf2.InvalidArgumentException,
+            tf2.LookupException,
+            tf2.TimeoutException,
+            tf2.TransformException,
+        ) as error:
+            self.get_logger().error(f"Failed to lookup transform: {error}")
+            return False, np.zeros(6)
+
+        # Get the goal end effector pose in base frame
+        try:
+            goal_pose.header.stamp = Time() # Get the most recent transform
+            goal_pose_base = self.tf_buffer.transform(goal_pose, self.BASE_LINK, timeout=Duration(seconds=timeout_secs))
+        except (
+            tf2.ConnectivityException,
+            tf2.ExtrapolationException,
+            tf2.InvalidArgumentException,
+            tf2.LookupException,
+            tf2.TimeoutException,
+            tf2.TransformException,
+        ) as error:
+            self.get_logger().error(f"Failed to transform goal pose: {error}")
+            return False, np.zeros(6)
+
+        # Get the error in base frame
+        err = np.zeros(6)
+        err[:3] = np.array([
+            goal_pose_base.pose.position.x - ee_transform.transform.translation.x,
+            goal_pose_base.pose.position.y - ee_transform.transform.translation.y,
+            goal_pose_base.pose.position.z - ee_transform.transform.translation.z,
+        ])
+        ee_orientation = euler_from_quaternion([
+            ee_transform.transform.rotation.x,
+            ee_transform.transform.rotation.y,
+            ee_transform.transform.rotation.z,
+            ee_transform.transform.rotation.w,
+        
+        ])
+        goal_orientation = euler_from_quaternion([
+            goal_pose_base.pose.orientation.x,
+            goal_pose_base.pose.orientation.y,
+            goal_pose_base.pose.orientation.z,
+            goal_pose_base.pose.orientation.w,
+        ])
+        err[3:] = np.array([
+            goal_orientation[0] - ee_orientation[0], # roll
+            goal_orientation[1] - ee_orientation[1], # pitch
+            goal_orientation[2] - ee_orientation[2], # yaw
+        ])
+
+        # Apply the mask
+        if use_mask:
+            err = np.where(self.cartesian_mask, err, 0.0)
+
+        return True, err
+
+
+
 
 def main(args: Optional[List[str]] = None):
     rclpy.init(args=args)
@@ -311,6 +651,9 @@ def main(args: Optional[List[str]] = None):
     executor = MultiThreadedExecutor(num_threads=4)
 
     rclpy.spin(move_to_pregrasp, executor=executor)
+
+    move_to_pregrasp.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
---
+++ nodes/test_controls.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+import subprocess
+import time
+import numpy as np
+
+from urdf import treeFromString
+import PyKDL as kdl
+
+def jacobian_to_np(J):
+    return np.array([[J[i,j] for j in range(J.columns())] for i in range(J.rows())])
+
+if __name__ == "__main__":
+    investigation_1 = False
+    if investigation_1:
+        # Investigation 1: Is it possible to get a chain and Jacobian
+        # without modifying the URDF?
+        urdf_string = subprocess.check_output([
+            "xacro",
+            "/home/hello-robot/ament_ws/src/stretch_ros2/stretch_description/"
+            "urdf/stretch_description_SE3_eoa_wrist_dw3_tool_sg3.xacro"
+        ])
+        # print(urdf_string.decode())
+        # raise Exception()
+        ok, robot = treeFromString(urdf_string)
+        # This chain has 8 non-fixed joints: joint_lift, joint_arm_l3, joint_arm_l2,
+        # joint_arm_l1, joint_arm_l0, joint_wrist_yaw, joint_wrist_pitch, joint_wrist_roll
+        orig_chain = robot.getChain("base_link", "link_grasp_center")
+        print(orig_chain, type(orig_chain), orig_chain.getNrOfJoints())
+
+        # # Add a dummy link for translation and rotation from the base link
+        # for i in range(orig_chain.getNrOfSegments()):
+        #     print("Segment", i)
+        #     s = orig_chain.getSegment(i)
+        #     print(" "*4, "Name", s.getName())
+        #     print(" "*4, "Joint", s.getJoint())
+        #     print(" "*4, "FrameToTip", s.getFrameToTip())
+        #     print(" "*4, "RigidBodyIntertia", s.getInertia().getMass(), s.getInertia().getCOG())
+        #     rot_intertia = s.getInertia().getRotationalInertia()
+        #     print(" "*4, "RotationalIntertia", [rot_intertia[i] for i in range(8)])
+        # test_rot_intertia = kdl.RotationalInertia(
+        #     Ixx=0.291286,
+        #     Iyy=0.004149,
+        #     Izz=-0.000174,
+        #     Ixy=0.001226,
+        #     Ixz=0.000354,
+        #     Iyz=0.291429
+        # )
+        # print("TestRotationalIntertia", [test_rot_intertia[i] for i in range(8)])
+        # Segment 6
+        #     Name link_arm_l0
+        #     Joint joint_arm_l0:[TransAxis, axis: [-3.80121e-15,-1.76235e-29,           1], origin[           0,           0,    -0.01375]]
+        #     FrameToTip [[           1,-2.62708e-15,-3.80121e-15;
+        # 2.62708e-15,           1,-1.76235e-29;
+        # 3.80121e-15, 7.63747e-30,           1]
+        # [           0,           0,    -0.01375]]
+        #     RigidBodyIntertia 0.427734 [    0.033681,   -0.000847,   -0.031723]
+        #     RotationalIntertia [0.0021197564274712923, -1.5797687000661998e-05, 0.000940017680375442, -1.5797687000661998e-05, 0.00302267519196166, -2.2492947812654e-05, 0.000940017680375442, -2.2492947812654e-05]
+        chain = kdl.Chain()
+        chain.addSegment(kdl.Segment(
+            name="link_base_translation",
+            joint=kdl.Joint(
+                name="joint_base_translation",
+                origin=kdl.Vector(0, 0, 0),
+                axis=kdl.Vector(1, 0, 0),
+                type=kdl.Joint.TransAxis,
+            ),
+            f_tip=kdl.Frame(),
+            I=kdl.RigidBodyInertia(
+                m=1.0,
+                oc=kdl.Vector(0, 0, 0),
+                Ic=kdl.RotationalInertia(
+                    1.0, 1.0, 1.0, 0.0, 0.0, 0.0
+                ),
+            ),
+        ))
+        chain.addSegment(kdl.Segment(
+            name="link_base_revolution",
+            joint=kdl.Joint(
+                name="joint_base_revolution",
+                origin=kdl.Vector(0, 0, 0),
+                axis=kdl.Vector(1, 0, 0),
+                type=kdl.Joint.RotAxis,
+            ),
+            f_tip=kdl.Frame(),
+            I=kdl.RigidBodyInertia(
+                m=1.0,
+                oc=kdl.Vector(0, 0, 0),
+                Ic=kdl.RotationalInertia(
+                    1.0, 1.0, 1.0, 0.0, 0.0, 0.0
+                ),
+            ),
+        ))
+        chain.addChain(orig_chain)
+
+        # Sample joints to test. Arm after homing.
+        # name:
+        # - joint_right_wheel
+        # - joint_left_wheel
+        # - joint_lift
+        # - joint_arm_l3
+        # - joint_arm_l2
+        # - joint_arm_l1
+        # - joint_arm_l0
+        # - joint_wrist_yaw
+        # - joint_head_pan
+        # - joint_head_tilt
+        # - joint_wrist_pitch
+        # - joint_wrist_roll
+        # - joint_gripper_finger_right
+        # - joint_gripper_finger_left
+        # position:
+        # - 0.0
+        # - 0.0
+        # - 0.5994133216606548
+        # - 0.024997924991407225
+        # - 0.024997924991407225
+        # - 0.024997924991407225
+        # - 0.024997924991407225
+        # - 0.009587379924285258
+        # - 0.007731993378606676
+        # - 0.06897162366246637
+        # - -0.5399612373357457
+        # - -0.0015339807878856412
+        # - 0.4450037904233133
+        # - 0.4450037904233133
+        q_kdl_1 = kdl.JntArray(chain.getNrOfJoints())
+        q_kdl_1[0] = 0.0
+        q_kdl_1[1] = 0.0
+        q_kdl_1[2] = 0.5994133216606548
+        q_kdl_1[3] = 0.024997924991407225
+        q_kdl_1[4] = 0.024997924991407225
+        q_kdl_1[5] = 0.024997924991407225
+        q_kdl_1[6] = 0.024997924991407225
+        q_kdl_1[7] = 0.009587379924285258
+        q_kdl_1[8] = -0.5399612373357457
+        q_kdl_1[9] = -0.0015339807878856412
+        # Arm with the wrist rotated in stow and lift lowered
+        # name:
+        # - joint_right_wheel
+        # - joint_left_wheel
+        # - joint_lift
+        # - joint_arm_l3
+        # - joint_arm_l2
+        # - joint_arm_l1
+        # - joint_arm_l0
+        # - joint_wrist_yaw
+        # - joint_head_pan
+        # - joint_head_tilt
+        # - joint_wrist_pitch
+        # - joint_wrist_roll
+        # - joint_gripper_finger_right
+        # - joint_gripper_finger_left
+        # position:
+        # - 0.0
+        # - 0.0
+        # - 0.1030816611928582
+        # - 0.024999421316935718
+        # - 0.024999421316935718
+        # - 0.024999421316935718
+        # - 0.024999421316935718
+        # - 3.1804535002162297
+        # - -1.5983458915376596
+        # - -1.0554362938577084
+        # - -0.007669903939428206
+        # - -0.0030679615757712823
+        # - 0.4450037904233133
+        # - 0.4450037904233133
+        q_kdl_2 = kdl.JntArray(chain.getNrOfJoints())
+        q_kdl_2[0] = 0.0
+        q_kdl_2[1] = 0.0
+        q_kdl_2[2] = 0.1030816611928582
+        q_kdl_2[3] = 0.024999421316935718
+        q_kdl_2[4] = 0.024999421316935718
+        q_kdl_2[5] = 0.024999421316935718
+        q_kdl_2[6] = 0.024999421316935718
+        q_kdl_2[7] = 3.1804535002162297
+        q_kdl_2[8] = -0.007669903939428206
+        q_kdl_2[9] = -0.0030679615757712823
+
+        # Get the Jacobian matrix for the chain
+        jac_solver = kdl.ChainJntToJacSolver(chain)
+        for q in [q_kdl_1, q_kdl_2]:
+            Js = []
+            for q01 in [(1.0, 3.14159/2), (q[0], q[1])]:
+                J = kdl.Jacobian(chain.getNrOfJoints())
+                q[0] = q01[0]
+                q[1] = q01[1]
+                jac_solver.JntToJac(q, J)
+                J = jacobian_to_np(J)
+                Js.append(J)
+            print(Js[0])
+            print(Js[1])
+            print(np.isclose(Js[0], Js[1], atol=1e-6))
+
+    investigation_2 = True
+    # Investigation 2: Try out controlling the robot using the velocity
+    # controller from visual servoing.
+    if investigation_2:
+        import normalized_velocity_control as nvc
+        import stretch_body.robot as rb
+
+        robot = rb.Robot()
+        robot.startup()
+
+        controller = nvc.NormalizedVelocityControl(robot)
+
+        zero_vel = {
+            'base_forward' : 0.0,
+            'base_counterclockwise' : 0.0,
+            'lift_up': 0.0,
+            'arm_out': 0.0,
+            'wrist_roll_counterclockwise' : 0.0,
+            'wrist_pitch_up' : 0.0,
+            'wrist_yaw_counterclockwise' : 0.0,
+            'head_pan_counterclockwise' : 0.0,
+            'head_tilt_up' : 0.0,
+            'gripper_open' : 0.0
+        }
+
+        vel = zero_vel.copy()
+        vel['lift_up'] = -0.1
+        controller.set_command(vel)
+        time.sleep(2.0)
+        controller.set_command(zero_vel)
+        time.sleep(1.0)
