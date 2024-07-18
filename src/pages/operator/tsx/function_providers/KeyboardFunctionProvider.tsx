083ad83fde3d46ab3f9a97144c595b271700d2ef
Amal Nanavati <amaln@hello-robot.com>
2024-07-17 17:15:04 -0700

    keyboard inputs integrated; minor bugs fixed

--- src/pages/operator/tsx/function_providers/KeyboardFunctionProvider.tsx
+++ src/pages/operator/tsx/function_providers/KeyboardFunctionProvider.tsx
@@ -53,7 +53,7 @@ export class KeyboardFunctionProvider extends FunctionProvider {
             break;
           case Mode.Wrist:
             console.log("a pressed; wrist mode");
-            keyInput = ButtonPadButton.WristRollLeft;
+            keyInput = ButtonPadButton.WristRotateIn;
             break;
           default:
             break;
@@ -89,11 +89,39 @@ export class KeyboardFunctionProvider extends FunctionProvider {
             break;
           case Mode.Wrist:
             console.log("d pressed; wrist mode");
+            keyInput = ButtonPadButton.WristRotateOut;
+            break;
+          default:
+            break;
+        }
+        break;
+      case "q":
+        switch (KeyboardFunction) {
+          case Mode.Wrist:
+            console.log("q pressed; wrist mode");
+            keyInput = ButtonPadButton.WristRollLeft;
+            break;
+          default:
+            break;
+        }
+        break;
+      case "e":
+        switch (KeyboardFunction) {
+          case Mode.Wrist:
+            console.log("e pressed; wrist mode");
             keyInput = ButtonPadButton.WristRollRight;
             break;
           default:
             break;
         }
+        break;
+      case "j":
+        console.log("j pressed; gripper closed");
+        keyInput = ButtonPadButton.GripperClose;
+        break;
+      case "k":
+        console.log("k pressed; gripper opened");
+        keyInput = ButtonPadButton.GripperOpen;
         break;
       case "ArrowUp":
         console.log("camera moved up");
@@ -111,6 +139,8 @@ export class KeyboardFunctionProvider extends FunctionProvider {
         console.log("camera moved right");
         keyInput = ButtonPadButton.CameraPanRight;
         break;
+      case "":
+        console.log("Key Released");
       default:
         console.log("Unmapped key", key);
         break;
@@ -122,145 +152,8 @@ export class KeyboardFunctionProvider extends FunctionProvider {
     keyboardFunction: Mode,
     key: string,
   ): ButtonFunctions {
+    // switch()
     let button = this.getButtonPadButton(keyboardFunction, key);
     return buttonFunctionProvider.provideFunctions(button);
   }
 }
-
-// export default function KeyboardFunctionProvider() {
-//   const [mode, setMode] = React.useState<Mode>();
-
-//   // constructor(){
-//   //   super();
-//   //   this.provideFunctions = this.provideFunctions.bind(this);
-//   // }
-
-//   // public provideFunctions()
-//   const handleKeyPress = React.useCallback(
-//     (event) => {
-//       switch (event.key) {
-//         case "w":
-//           HandleW();
-//           break;
-//         case "a":
-//           HandleA();
-//           break;
-//         case "s":
-//           HandleS();
-//           break;
-//         case "d":
-//           HandleD();
-//           break;
-//         case "1":
-//           setMode(Mode.Base);
-//           console.log("base mode enabled");
-//           break;
-//         case "2":
-//           setMode(Mode.Arm);
-//           console.log("arm mode enabled");
-//           break;
-//         case "3":
-//           setMode(Mode.Wrist);
-//           console.log("wrist mode enabled");
-//           break;
-//         default:
-//           break;
-//       }
-
-//       switch (event.keyCode) {
-//         case 38:
-//           console.log("camera moved up");
-//           break;
-//         case 37:
-//           console.log("camera moved left");
-//           break;
-//         case 40:
-//           console.log("camera moved down");
-//           break;
-//         case 39:
-//           console.log("camera moved right");
-//           break;
-//       }
-//     },
-//     [mode],
-//   );
-
-//   const HandleW = () => {
-//     switch (mode) {
-//       case Mode.Base:
-//         console.log("w pressed; base mode");
-//         break;
-//       case Mode.Arm:
-//         console.log("w pressed; arm mode");
-//         break;
-//       case Mode.Wrist:
-//         console.log("w pressed; wrist mode");
-//         break;
-//       default:
-//         break;
-//     }
-//   };
-
-//   const HandleA = () => {
-//     switch (mode) {
-//       case Mode.Base:
-//         console.log("a pressed; base mode");
-//         break;
-//       case Mode.Arm:
-//         console.log("a pressed; arm mode");
-//         break;
-//       case Mode.Wrist:
-//         console.log("a pressed; wrist mode");
-//         break;
-//       default:
-//         break;
-//     }
-//   };
-
-//   const HandleS = () => {
-//     switch (mode) {
-//       case Mode.Base:
-//         console.log("s pressed; base mode");
-//         break;
-//       case Mode.Arm:
-//         console.log("s pressed; arm mode");
-//         break;
-//       case Mode.Wrist:
-//         console.log("s pressed; wrist mode");
-//         break;
-//       default:
-//         break;
-//     }
-//   };
-
-//   const HandleD = () => {
-//     switch (mode) {
-//       case Mode.Base:
-//         console.log("d pressed; base mode");
-//         break;
-//       case Mode.Arm:
-//         console.log("d pressed; arm mode");
-//         break;
-//       case Mode.Wrist:
-//         console.log("d pressed; wrist mode");
-//         break;
-//       default:
-//         break;
-//     }
-//   };
-
-//   React.useEffect(() => {
-//     window.addEventListener("keydown", handleKeyPress);
-//     return () => {
-//       window.removeEventListener("keydown", handleKeyPress);
-//     };
-//   }, [handleKeyPress]);
-
-//   return (
-//     <>
-//       <div>
-//         <button>{mode}</button>
-//       </div>
-//     </>
-//   );
-// }
--- src/pages/operator/tsx/layout_components/KeyboardControl.tsx
+++ src/pages/operator/tsx/layout_components/KeyboardControl.tsx
@@ -9,6 +9,7 @@ import { Mode } from "../function_providers/KeyboardFunctionProvider";
 
 export const KeyboardControl = (props: CustomizableComponentProps) => {
   const [mode, setMode] = React.useState<Mode>(Mode.Base);
+  const [keyPressed, setKeyPressed] = React.useState<boolean>(false);
 
   const { customizing } = props.sharedState;
   const selected = isSelected(props);
@@ -21,33 +22,56 @@ export const KeyboardControl = (props: CustomizableComponentProps) => {
 
   const handleKeyPress = React.useCallback(
     (event) => {
+      if (keyPressed === true) {
+        return;
+      }
+      console.log(keyPressed);
+
+      setKeyPressed(true);
+
       switch (event.key) {
         case "1":
           setMode(Mode.Base);
           console.log("base mode enabled");
           break;
         case "2":
-          setMode(Mode.Arm);
-          console.log("arm mode enabled");
-          break;
-        case "3":
           setMode(Mode.Wrist);
           console.log("wrist mode enabled");
+          break;
+        case "3":
+          setMode(Mode.Arm);
+          console.log("arm mode enabled");
           break;
       }
 
       let functs = keyboardFunctionProvider.provideFunctions(mode, event.key);
       functs.onClick();
     },
-    [mode],
+    [mode, keyPressed],
+  );
+
+  const handleKeyRelease = React.useCallback(
+    (event) => {
+      console.log("Key Released");
+      setKeyPressed(false);
+      let functs = keyboardFunctionProvider.provideFunctions(mode, event.key);
+      functs.onRelease();
+    },
+    [mode, keyPressed],
   );
 
   React.useEffect(() => {
+    //window.onkeydown  = handleKeyPress;
+    // window.onkeyup = function(){
+    //   this.onkeydown = handleKeyPress;
+    // }
     window.addEventListener("keydown", handleKeyPress);
+    window.addEventListener("keyup", handleKeyRelease);
     return () => {
       window.removeEventListener("keydown", handleKeyPress);
+      window.removeEventListener("keyup", handleKeyRelease);
     };
-  }, [handleKeyPress]);
+  }, [handleKeyPress, handleKeyRelease]);
 
   return (
     <>
