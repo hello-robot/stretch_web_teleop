c2285f64097aa397f3777d4f76f43be3c3f492dc
Hello Robot <clvdeployment@gmail.com>
2024-08-01 08:01:30 -0700

    Also set PCM and Mic as part of default audio configuration

--- configure_audio.sh
+++ configure_audio.sh
@@ -11,13 +11,15 @@
 # robot's built-in microphone. The only way to use the robot's built-in microphone
 # is to disconnect all external microphones.
 SPEAKER_NAME="alsa_output.pci-0000_00_1f.3.analog-stereo"
-if getopts ":s:" opt && [[ $opt == "s" ]]; then
-    SPEAKER_NAME="$OPTARG"
-fi
 MIC_NAME=""
-if getopts ":m:" opt && [[ $opt == "m" ]]; then
-    MIC_NAME="$OPTARG"
-fi
+while getopts ":s:m:" opt
+   do
+     case $opt in
+        s ) SPEAKER_NAME="$OPTARG";;
+        m ) MIC_NAME="$OPTARG";;
+     esac
+done
+
 
 echo "Configuring audio...Note that this script may not work if X-11 forwarding is enabled."
 
@@ -29,12 +31,20 @@ if [ ${#MIC_NAME} -gt 0 ]; then
     pactl set-default-source $MIC_NAME
 fi
 
-echo "Unmuting the speaker and setting its volume to 100%"
+echo "Unmuting the speaker (Master) and setting its volume to 100%"
 amixer set 'Master' unmute -q
 amixer sset 'Master' 100% -q
 
-echo "Unmuting the microphone and setting its gain to 100%"
+echo "Unmuting the speaker (PCM) and setting its volume to 100%"
+amixer set 'PCM' unmute -q
+amixer sset 'PCM' 100% -q
+
+echo "Unmuting the microphone (Capture) and setting its gain to 100%"
 amixer set 'Capture' cap -q
 amixer sset 'Capture' 100% -q
+
+echo "Unmuting the microphone (Mic) and setting its gain to 100%"
+amixer set 'Mic' cap -q
+amixer sset 'Mic' 100% -q
 
 echo "Done configuring audio!"
