b16a1ce689109538d04b65e008ac7b9161fdf001
Amal Nanavati <amaln@hello-robot.com>
2024-08-09 14:08:29 -0700

    Expand configure audio to work with multiple audio devices

--- configure_audio.sh
+++ configure_audio.sh
@@ -10,31 +10,65 @@
 # only one external mic plugged in; that one will **always** be prioritized over the
 # robot's built-in microphone. The only way to use the robot's built-in microphone
 # is to disconnect all external microphones.
+
+
+# Configure command line arguments
 SPEAKER_NAME="alsa_output.pci-0000_00_1f.3.analog-stereo"
-if getopts ":s:" opt && [[ $opt == "s" ]]; then
-    SPEAKER_NAME="$OPTARG"
-fi
 MIC_NAME=""
-if getopts ":m:" opt && [[ $opt == "m" ]]; then
-    MIC_NAME="$OPTARG"
-fi
+while getopts ":s:m:" opt
+do
+    case $opt in
+        s ) SPEAKER_NAME="$OPTARG" ;;
+        m ) MIC_NAME="$OPTARG" ;;
+    esac
+done
+
 
 echo "Configuring audio...Note that this script may not work if X-11 forwarding is enabled."
 
-echo "Setting speaker to $SPEAKER_NAME"
-pactl set-default-sink $SPEAKER_NAME
-
-if [ ${#MIC_NAME} -gt 0 ]; then
-    echo "Setting microphone to $MIC_NAME"
-    pactl set-default-source $MIC_NAME
+# Set the default speaker
+sinks=$(pactl list short sinks)
+if [[ $sinks == *"$SPEAKER_NAME"* ]]; then
+    echo "    Setting speaker to $SPEAKER_NAME"
+    pactl set-default-sink $SPEAKER_NAME
+else
+    echo "    Speaker name is invalid"
+    echo "    To get valid speaker names, run:"
+    echo "        pactl list short sinks"
 fi
 
-echo "Unmuting the speaker and setting its volume to 100%"
-amixer set 'Master' unmute -q
-amixer sset 'Master' 100% -q
+# Set the default microphone
+if [ ${#MIC_NAME} -gt 0 ]; then
+    sources=$(pactl list short sources)
+    if [[ $sources == *"$MIC_NAME"* ]]; then
+        echo "    Setting microphone to $MIC_NAME"
+        pactl set-default-source $MIC_NAME
+    else
+        echo "    Microphone name is invalid"
+        echo "    To get valid microphone names, run:"
+        echo "        pactl list short sources"
+    fi
+fi
 
-echo "Unmuting the microphone and setting its gain to 100%"
-amixer set 'Capture' cap -q
-amixer sset 'Capture' 100% -q
+# Unmute and set volume to 100% for relevant audio devices
+amixer_output=$(amixer scontrols)
+amixer_devices=('Master' 'Mic' 'Capture' 'PCM' 'Digital')
+for device in "${amixer_devices[@]}"
+do
+    if [[ $amixer_output == *"'$device'"* ]]; then
+        echo "    Unmuting '$device'"
+        device_output=$(amixer get $device)
+        if [[ $device_output == *"[off]"* ]]; then
+            if [[ $device_output == *"Cap"* ]]; then
+                amixer set $device cap -q
+            else
+                amixer set $device unmute -q
+            fi
+        fi
+
+        echo "    Setting '$device' to 100%"
+        amixer sset $device 100% -q
+    fi
+done
 
 echo "Done configuring audio!"
