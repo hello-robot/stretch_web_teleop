d21087bb45faead897b4e9917d56a6f0f9d53535
Binit Shah <bshah@hello-robot.com>
2024-09-25 22:44:00 -0400

    Test do browser pages connect

--- tests/signaling.spec.ts
+++ tests/signaling.spec.ts
@@ -1,16 +1,39 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { exec, execSync } from 'child_process';
 
-test('has title', async ({ browser }) => {
-    const playwrightContext = await browser.newContext();
-    const githubContext = await browser.newContext();
-    try {
-        const playwrightPage = await playwrightContext.newPage();
-        const githubPage = await githubContext.newPage();
-        await playwrightPage.goto('https://playwright.dev/');
-        await githubPage.goto('https://github.com/');
-        await expect(playwrightPage).toHaveTitle(/Playwright/);
-        await expect(githubPage).toHaveTitle(/GitHub/);
-    } finally {
-        await playwrightContext.close()
-    }
+test.describe('local signaling', () => {
+    let server_process;
+
+    test.beforeAll(async () => {
+        server_process = exec('node server.js');
+        execSync('sleep 1');
+    });
+
+    test.afterAll(async () => {
+        server_process.kill();
+    });
+
+    test('do browser pages connect', async ({ browser }) => {
+        const robotContext = await browser.newContext();
+        const operatorContext = await browser.newContext();
+        try {
+            const robotPage = await robotContext.newPage();
+            const operatorPage = await operatorContext.newPage();
+
+            robotPage.on('console', (msg) => {
+                console.debug('Robot browser said:', msg.text());
+            });
+            operatorPage.on('console', (msg) => {
+                console.debug('Operator browser said:', msg);
+            });
+
+            await robotPage.goto('https://localhost/robot/');
+            await operatorPage.goto('https://localhost/operator/');
+
+            execSync('sleep 2');
+        } finally {
+            await robotContext.close();
+            await operatorContext.close();
+        }
+    });
 });
