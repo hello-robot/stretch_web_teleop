3cdfee7fcd68a3e4ef94f3a155d917846c09ac11
Binit Shah <bshah@hello-robot.com>
2024-10-21 17:15:23 -0400

    Fill out caller card in robot selector

--- src/pages/home/tsx/components/CallRobotSelector.tsx
+++ src/pages/home/tsx/components/CallRobotSelector.tsx
@@ -3,46 +3,69 @@ import React, { useEffect, useState } from "react";
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+import CardActionArea from "@mui/material/CardActionArea";
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
+import CircleIcon from "@mui/icons-material/Circle";
+import { green, red } from '@mui/material/colors';
 
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    ...theme.applyStyles('dark', {
-      backgroundColor: '#1A2027',
-    }),
-}));
-
-const CallRobotItem = () => {
+const CallRobotItem = (props: {
+    name: String;
+    isOnline: Boolean;
+}) => {
 
     return (
         <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-                <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-                    Word of the Day
-                </Typography>
-                <Typography variant="h5" component="div">
-                    benevolent
-                </Typography>
-                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
-                <Typography variant="body2">
-                    well meaning and kindly.
-                    <br />
-                    {'"a benevolent smile"'}
-                </Typography>
-                <CardActions>
-                    <Button size="small">Learn More</Button>
-                </CardActions>
-            </CardContent>
+            {props.isOnline ? (
+                <CardActionArea onClick={() => console.log("CardActionArea clicked")}>
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            {props.name}
+                        </Typography>
+                        <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>
+                            <CircleIcon sx={{
+                                fontSize: 12,
+                                "@keyframes glowing_green": {
+                                    "0%": {
+                                        color: green["A400"]
+                                    },
+                                    "50%": {
+                                        color: green["A200"]
+                                    },
+                                    "100%": {
+                                        color: green["A400"]
+                                    }
+                                },
+                                color: green["A400"],
+                                animation: "glowing_green 3s linear infinite" }} /> Online
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            ) : (
+                <CardContent>
+                    <Typography variant="h5" component="div">
+                        {props.name}
+                    </Typography>
+                    <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>
+                        <CircleIcon sx={{
+                            fontSize: 12,
+                            "@keyframes glowing_red": {
+                                "0%": {
+                                    color: red["A400"]
+                                },
+                                "50%": {
+                                    color: red["A200"]
+                                },
+                                "100%": {
+                                    color: red["A400"]
+                                }
+                            },
+                            color: red["A400"],
+                            animation: "glowing_red 3s linear infinite" }} /> Offline
+                    </Typography>
+                </CardContent>
+            )}
         </Card>
     );
 };
@@ -57,13 +80,10 @@ export const CallRobotSelector = (props: {
             <h2>Robots:</h2>
             <Grid container spacing={2} className='rs-container' style={props.style}>
                 <Grid size={{md: 12, lg: 6}}>
-                    <CallRobotItem />
+                    <CallRobotItem name="stretch-se3-3004" isOnline={true} />
                 </Grid>
                 <Grid size={{md: 12, lg: 6}}>
-                    <CallRobotItem />
-                </Grid>
-                <Grid size={{md: 12, lg: 6}}>
-                    <CallRobotItem />
+                    <CallRobotItem name="stretch-re1-1002" isOnline={false} />
                 </Grid>
             </Grid>
         </Box>
