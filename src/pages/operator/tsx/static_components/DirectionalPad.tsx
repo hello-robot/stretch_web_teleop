c07a5a5820e8f0b6b1f340d065b3904a676a29bc
hello-william <email@wle8300.com>
2025-05-28 13:00:28 -0700

    break out the directional buttonpad into its own file; add turn buttons;

---
+++ src/pages/operator/tsx/static_components/DirectionalPad.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import 'operator/css/DirectionalPad.css'
+
+export default function DirectionalPad({ mapButtons }) {
+  const buttons = ['north', 'south', 'west', 'east'];
+  return (
+    <div className="base-movement-controls">
+      <svg className="mask">
+        <clipPath id="clip-path" clipPathUnits="objectBoundingBox"><path d="M1,1 H0 V0.5 C0.276,0.5,0.5,0.276,0.5,0 H1 V1"></path></clipPath>
+      </svg>
+      <div className="button-turn-wrapper">
+        <button className="button-turn left" aria-label="Turn left">
+        </button>
+        <div className='line' />
+        <button className="button-turn right" aria-label="Turn right">
+        </button>
+      </div>
+      <div className="dbutton-pad">
+        {buttons.map(mapButtons)}
+      </div>
+    </div>
+  )
+}
