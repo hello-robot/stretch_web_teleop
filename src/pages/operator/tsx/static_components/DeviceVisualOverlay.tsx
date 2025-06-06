cbed50fc4231cfd20c1dd5a2936236515acc41a5
hello-william <email@wle8300.com>
2025-06-06 16:21:07 -0700

    add files

---
+++ src/pages/operator/tsx/static_components/DeviceVisualOverlay.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "operator/css/DeviceVisualOverlay.css";
+
+export default function DeviceVisualOverlay() {
+    return (
+        <div className="safety-overlay" />
+    )
+}
